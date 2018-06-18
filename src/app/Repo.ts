31f16160c5582d92f95d7df87f24e2145d98c61e
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2018-06-18 19:13:05 +0000

    add docker.

--- src/app/Repo.ts
+++ src/app/Repo.ts
@@ -3,11 +3,13 @@ export class Repo {
   name: String;
   description: String;
   pushed_at: Date;
+  dockerImage: boolean;
 
   constructor(name: string, description: string, pushed_at: string) {
     this.name = name;
     this.description = description;
     this.pushed_at = new Date(pushed_at);
+    this.dockerImage = this.name.includes('docker');
   }
 
 }
