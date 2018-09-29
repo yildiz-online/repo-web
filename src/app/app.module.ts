eda724ea9c12e6d41cf3bb8d0e705fc7ef53aa07
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2018-09-29 10:55:19 +0200

    [YE-0] Use ng-bootstrap.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import {RepoService} from './repo.service';
@@ -12,7 +12,8 @@ import {HttpClient, HttpClientModule} from '@angular/common/http';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    NgbModule
   ],
   providers: [RepoService, HttpClient],
   bootstrap: [AppComponent]
