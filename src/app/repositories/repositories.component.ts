c234f44099f65e0efb837b2b17432eb82bae2971
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2019-11-25 13:39:31 +0100

    Update repositories.component.ts

--- src/app/repositories/repositories.component.ts
+++ src/app/repositories/repositories.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {RepositoriesService} from './repositories.service';
+import {CommitsService} from './commits.service';
 import {Observable} from 'rxjs';
 import 'rxjs/add/observable/of';
 import {Meta} from "@angular/platform-browser";
@@ -16,7 +17,7 @@ export class RepositoriesComponent implements OnInit {
   content: Observable<Repository[]>;
   repositoryPresentation: RepositoryPresentation;
 
-  constructor(private repoService: RepositoriesService, private meta: Meta) {
+  constructor(private repoService: RepositoriesService, private commitService: CommitsService, private meta: Meta) {
     this.meta.updateTag({ name: 'description', content: 'The list of all the open-source repositories for the game engine Yildiz-Engine'})
   }
 
