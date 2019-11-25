c4531b7c0a678b4776b37ab15d3ebfbbf8726130
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2019-11-25 11:13:34 +0100

    Update Repository.ts

--- src/app/repositories/Repository.ts
+++ src/app/repositories/Repository.ts
@@ -3,11 +3,13 @@ export class Repository {
   name: String;
   description: String;
   pushed_at: Date;
+  open_issues_count: String;
 
-  constructor(name: string, description: string, pushed_at: string) {
+  constructor(name: string, description: string, pushed_at: string, open_issues_count: string) {
     this.name = name;
     this.description = description;
     this.pushed_at = new Date(pushed_at);
+    this.open_issues_count = open_issues_count;
   }
 
 }
