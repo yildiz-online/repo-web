8cfb0e71b93c7f17af38e08192ed49e0a294b2b5
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2019-11-25 13:25:26 +0100

    Update commits.service.ts

--- src/app/repositories/commits.service.ts
+++ src/app/repositories/commits.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {Repository} from './Commit';
+import {Commit} from './Commit';
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/expand';
